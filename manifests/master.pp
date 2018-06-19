3d216b1c679a41769f3ff249c6aae2511e4918be
Julien Morot <jmorot@gmail.com>
2018-06-19 22:40:02 +0200

    add defaults organizational units

--- manifests/master.pp
+++ manifests/master.pp
@@ -21,11 +21,6 @@ class ldapauth::master(String $basedn = 'dc=domain,dc=tld', String $rootpw = 'no
 	    require => Openldap::Server::Schema["inetorgperson"],
 	}
 
-#    file { '/tmp/provider_sync.ldif':
-#        ensure => present,
-#        source => 'puppet:///modules/ldap_interne/provider_sync.ldif'
-#    }
-
     file { '/var/lib/ldap/accesslog':
         ensure => directory,
         owner => "openldap",
@@ -35,8 +30,27 @@ class ldapauth::master(String $basedn = 'dc=domain,dc=tld', String $rootpw = 'no
     file { '/var/lib/ldap/accesslog/DB_CONFIG':
         ensure => present,
         owner => "openldap",
-        source => "puppet:///modules/ldapauth/DB_CONFIG",
+        source => "puppet:///modules/${module_name}/DB_CONFIG",
         require => File['/var/lib/ldap/accesslog']
     }
+
+    file { '/root/.ldap_defaults_ou.ldif':
+        ensure  => file,
+        content => template('ldapauth/defaults_ou.ldif.erb'),
+        owner   => '0',
+        group   => '0',
+        mode    => '0600',
+        notify  => Exec['add_defaults_ou'],
+    }
+    exec { 'add_defaults_ou':
+        command  => "ldapadd -c -x -D cn=admin,${basedn} -w ${rootpw} -f /root/.ldap_defaults_ou.ldif && touch /root/.ldap_defaults_ou.done",
+        path     => '/usr/bin:/usr/sbin:/bin:/sbin',
+        provider => shell,
+        onlyif   => ['test -f /root/.ldap_defaults_ou.ldif'],
+        unless   => ['test -f /root/.ldap_defaults_ou.done'],
+    }
+
+
+
 
 }
