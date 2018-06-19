c04b43459cfc0009844229d1d14070a6c0c5132b
Julien Morot <jmorot@gmail.com>
2018-06-19 22:17:03 +0200

    add db_config to accesslog

--- manifests/master.pp
+++ manifests/master.pp
@@ -8,17 +8,35 @@ class ldapauth::master(String $basedn = 'dc=domain,dc=tld', String $rootpw = 'no
     }
 	openldap::server::schema { 'cosine':
 	    ensure  => present,
-	    path    => '${ldapauth::params::slapdconfpath}/schema/cosine.schema',
+	    path    => "${ldapauth::params::slapdconfpath}/schema/cosine.schema",
 	}
 	openldap::server::schema { 'inetorgperson':
   	    ensure  => present,
-	    path    => '${ldapauth::params::slapdconfpath}/schema/inetorgperson.schema',
+	    path    => "${ldapauth::params::slapdconfpath}/schema/inetorgperson.schema",
 	    require => Openldap::Server::Schema["cosine"],
 	}
 	openldap::server::schema { 'nis':
 	    ensure  => present,
-	    path    => '${ldapauth::params::slapdconfpath}/schema/nis.ldif',
+	    path    => "${ldapauth::params::slapdconfpath}/schema/nis.ldif",
 	    require => Openldap::Server::Schema["inetorgperson"],
 	}
+
+#    file { '/tmp/provider_sync.ldif':
+#        ensure => present,
+#        source => 'puppet:///modules/ldap_interne/provider_sync.ldif'
+#    }
+
+    file { '/var/lib/ldap/accesslog':
+        ensure => directory,
+        owner => "openldap",
+        group => "openldap",
+    }
+
+    file { '/var/lib/ldap/accesslog/DB_CONFIG':
+        ensure => present,
+        owner => "openldap",
+        source => "puppet:///modules/ldapauth/DB_CONFIG",
+        require => File['/var/lib/ldap/accesslog']
+    }
 
 }
