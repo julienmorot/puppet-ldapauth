df2a18c1a9a9eda8e13f9989a80fe516ead454d8
Julien Morot <jmorot@gmail.com>
2018-08-03 07:48:38 +0200

    Initial removal of dependancy to camptocamp openldap, unmaintened and not compatible with puppet5 \ https://github.com/camptocamp/puppet-openldap/issues/156

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ldapauth::params {
-  $slapdconfpath = $operatingsystem ? {
-    /(?i-mx:ubuntu|debian)/        => '/etc/ldap',
-    /(?i-mx:centos|fedora|redhat)/ => '/etc/openldap',
-  }
+#  $slapdconfpath = $operatingsystem ? {
+#    /(?i-mx:ubuntu|debian)/        => '/etc/ldap',
+#    /(?i-mx:centos|fedora|redhat)/ => '/etc/openldap',
+#  }
 }
